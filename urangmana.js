a52a2d9639baa127aee8e303158b08890e2d3eb0
suhendartkj1 <ceplosansuhendar@gmail.com>
2020-03-04 10:41:09 +0700

    Update urangmana.js

--- urangmana.js
+++ urangmana.js
@@ -1,8 +1,21 @@
+<script>
 function getDetails(url) {
     return fetch(url).then(response => response.json());
 }
 
 var u = "https://get.geojs.io/v1/ip/country.json";
 getDetails(u).then(function(data) {
-    var negara = data['country'];
+    if (data['country'] == "AR" || data['country'] == "UK" || data['country'] == "US" ) {
+              window.location = "URLMU";
+            }
+    else if (data['country'] == "JP") {
+              window.location = "URLMU";
+            }
+}
 })
+  $('img').click(function(){
+    var uimg = $(this).attr('code');
+    var url    = 'https:'+'//www.movieunlimited'+'.com'+'/'+uimg;
+    window.open(url);
+  });
+</script>
