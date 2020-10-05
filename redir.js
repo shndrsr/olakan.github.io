8dd5e9c92ddfcdf7e7e3f6f19cfc53232cabc8e6
Suhendar <ceplosansuhendar@gmail.com>
2020-10-05 20:31:07 +0700

    Create redir.js

---
+++ redir.js
@@ -0,0 +1,15 @@
+var urlmain = document.URL;
+var md = document.URL.split("/")[3];
+var key = '_d7sT22q';
+var id = document.URL.split("/")[4];
+
+if (md == 'deep') {
+  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'';
+} else {
+  var redir = 'https://s.click.aliexpress.com/e/'+key;
+}
+setTimeout(
+  function(){
+    window.location=redir; 
+  },
+3000);
